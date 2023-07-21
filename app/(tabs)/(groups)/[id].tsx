17210764ecf64afbb7a23b5605fef726b2dfc9d1
Brian Ortega <brian.ort02@icloud.com>
2023-07-20 21:57:36 -0500

    Chat working

--- app/(tabs)/(groups)/[id].tsx
+++ app/(tabs)/(groups)/[id].tsx
@@ -1,17 +1,152 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
+import { FIRESTORE_DB } from "@/config/FirebaseConfig";
+import {
+  DocumentData,
+  onSnapshot,
+  collection,
+  addDoc,
+  serverTimestamp,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { useState, useLayoutEffect } from "react";
+import { useAuth } from "@/context/AuthContext";
 
 const ChatPage = () => {
-  const handlePress = () => {
-    console.log("Pressed");
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const { user } = useAuth();
+  const [messages, setMessages] = useState<DocumentData>([]);
+  const [message, setMessage] = useState("");
+
+  useLayoutEffect(() => {
+    const msgCollectionRef = collection(FIRESTORE_DB, `groups/${id}/messages`);
+    const q = query(msgCollectionRef, orderBy("createdAt", "asc"));
+
+    const unsubscribe = onSnapshot(q, (groups: DocumentData) => {
+      const messages = groups.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      console.log("Current messages:", messages);
+
+      setMessages(messages);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const sendMessage = async () => {
+    const msg = message.trim();
+    if (msg.length === 0) return;
+
+    const msgCollectionRef = collection(FIRESTORE_DB, `groups/${id}/messages`);
+
+    await addDoc(msgCollectionRef, {
+      message: msg,
+      sender: user.uid,
+      createdAt: serverTimestamp(),
+    });
+    setMessage("");
+  };
+
+  const renderMessage = ({ item }: { item: DocumentData }) => {
+    const myMessage = item.sender === user.uid;
+    return (
+      <View
+        style={[
+          styles.messageContainer,
+          myMessage
+            ? styles.userMessageContainer
+            : styles.otherMessageContainer,
+        ]}
+      >
+        <Text style={styles.messageText}>{item.message}</Text>
+        <Text style={styles.time}>
+          {item.createdAt?.toDate().toLocaleDateString()}
+        </Text>
+      </View>
+    );
   };
 
   return (
-    <View>
-      <Text onPress={handlePress}>id</Text>
-    </View>
+    // <KeyboardAvoidingView style={styles.container} behavior={Platform.OS ==== 'ios' ? 'padding' : 'height'}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={100}
+    >
+      <FlatList
+        data={messages}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMessage}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={message}
+          onChangeText={(text) => setMessage(text)}
+          style={styles.messageInput}
+          placeholder="hello"
+          placeholderTextColor="#ccc"
+          multiline
+        />
+        <Button disabled={message === ""} title="Send" onPress={sendMessage} />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    padding: 10,
+    gap: 10,
+    backgroundColor: "#fff",
+  },
+  messageInput: {
+    flex: 1,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  messageContainer: {
+    padding: 10,
+    marginTop: 10,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    maxWidth: "80%",
+  },
+  userMessageContainer: {
+    backgroundColor: "#dcf8c6",
+    alignSelf: "flex-end",
+  },
+  otherMessageContainer: {
+    backgroundColor: "#fff",
+  },
+  messageText: {
+    fontSize: 16,
+  },
+  time: {
+    fontSize: 12,
+    color: "#777",
+    alignSelf: "flex-end",
+  },
+});
 export default ChatPage;
--- app/(tabs)/(groups)/index.tsx
+++ app/(tabs)/(groups)/index.tsx
@@ -1,19 +1,126 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
 
-import React from "react";
-import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import { Link, useRouter } from "expo-router";
+import {
+  DocumentData,
+  addDoc,
+  collection,
+  onSnapshot,
+} from "firebase/firestore";
 
-const groups = () => {
-  const router = useRouter();
-  const handlePress = () => {
-    console.log("Pressed");
-    router.push("/123");
+import { FIRESTORE_DB } from "@/config/FirebaseConfig";
+import { useAuth } from "@/context/AuthContext";
+import { ScrollView } from "react-native-gesture-handler";
+
+const GroupsPage = () => {
+  const [groupCollectionRef, setGroupsCollectionRef] = React.useState(null);
+  const [groups, setGroups] = React.useState([]);
+  const { user } = useAuth();
+  useEffect(() => {
+    const ref = collection(FIRESTORE_DB, "groups");
+    setGroupsCollectionRef(ref);
+
+    const unsubscribe = onSnapshot(ref, (groups: DocumentData) => {
+      console.log("current data", groups);
+      const groupsdata = groups.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      console.log(groupsdata);
+
+      setGroups(groupsdata);
+    });
+    return unsubscribe;
+  }, []);
+
+  const startGroup = async () => {
+    console.log("StartGroup");
+    try {
+      await addDoc(groupCollectionRef, {
+        name: `Group #${Math.floor(Math.random() * 1000)}`,
+        description: "This is a chat room",
+        creator: user.uid,
+      });
+    } catch (error) {
+      console.log("Error");
+    }
   };
+
+  const router = useRouter();
+  // const handlePress = () => {
+  //   console.log("Pressed");
+  //   router.push("/123");
+  // };
   return (
-    <View>
-      <Text onPress={handlePress}>groups</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        {groups.map((group) => {
+          return (
+            <Link key={group.id} href={`/(groups)/${group.id}`} asChild>
+              <TouchableOpacity style={styles.groupCard}>
+                <Text>{group.name}</Text>
+                <Text>{group.description}</Text>
+              </TouchableOpacity>
+            </Link>
+            // <Pressable
+            //   key={group.id}
+            //   style={{ gap: 12, backgroundColor: "red", margin: 12 }}
+            //   onPress={() => {
+            //     console.log("Pressed");
+            //     router.push(`/groups/${group.id}`);
+            //   }}
+            // >
+            //   <View style={{ gap: 12 }}>
+            //     <Text style={{}}>{group.name}</Text>
+            //     <Text style={{}}>{group.description}</Text>
+            //   </View>
+            // </Pressable>
+          );
+        })}
+      </ScrollView>
+
+      <Pressable onPress={startGroup} style={styles.fab}>
+        <Text style={{ color: "white", fontSize: 24 }}>+</Text>
+      </Pressable>
     </View>
   );
 };
 
-export default groups;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fab: {
+    position: "absolute",
+    width: 56,
+    height: 56,
+    alignItems: "center",
+    justifyContent: "center",
+    right: 20,
+    bottom: 20,
+    backgroundColor: "#03A9F4",
+    borderRadius: 30,
+    elevation: 8,
+  },
+  groupCard: {
+    padding: 12,
+    marginHorizontal: 12,
+    marginTop: 12,
+    marginBottom: 4,
+    backgroundColor: "white",
+    borderRadius: 12,
+
+    // elevation: 4,
+  },
+});
+
+export default GroupsPage;
