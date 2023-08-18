dc75d40493ff6aeaf3bdb73bcc124d4eec6e5bad
Brian Ortega <brian.ort02@icloud.com>
2023-08-18 16:56:20 -0500

    working payment intent
    payment intent goes to firebase. Currently called "payments"

--- app/(tabs)/profile/marketplace.tsx
+++ app/(tabs)/profile/marketplace.tsx
@@ -1,14 +1,32 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Button, ButtonText } from "@gluestack-ui/react";
+import axios from "axios";
+import { useStripe, StripeProvider } from "@stripe/stripe-react-native";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  setDoc,
+  getDoc,
+  addDoc,
+} from "firebase/firestore";
+import { FIRESTORE_DB } from "@/config/FirebaseConfig";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Marketplace() {
+  const userId = useAuth().user?.uid;
+  console.log("userId", userId);
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [isLoading, setLoading] = useState(false);
   const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: "lightblue",
     },
+    margins: { marginHorizontal: 12, paddingTop: 12 },
     personContainer: {
-      margin: 12,
+      paddingVertical: 12,
       backgroundColor: "white",
       borderRadius: 12,
       padding: 12,
@@ -18,15 +36,49 @@ export default function Marketplace() {
     },
     details: {},
   });
-  const handleButton = () => {
-    console.log("Pressed me");
+  const createPaymentDocument = async (userId, paymentData) => {
+    try {
+      const paymentsCollectionRef = collection(
+        FIRESTORE_DB,
+        `stripe_customers/${userId}/payments`
+      );
+      await addDoc(paymentsCollectionRef, paymentData);
+      console.log("Payment document created successfully");
+    } catch (error) {
+      console.error("Error creating payment document:", error);
+    }
   };
+
+  // Usage example
+  const handleCheckout = async () => {
+    try {
+      setLoading(true);
+      const paymentData = {
+        amount: 1000, // Example amount in cents
+        currency: "usd", // Example currency
+        // Add any other relevant payment data
+      };
+      await createPaymentDocument(userId, paymentData);
+    } catch (error) {
+      console.error("Error initiating payment:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Pressable style={styles.personContainer} onPress={handleButton}>
-        <Text style={styles.person}>Person</Text>
-        <Text style={styles.details}>total: $45</Text>
-      </Pressable>
+      <View style={styles.margins}>
+        <Pressable style={styles.personContainer}>
+          <Text style={styles.person}>Person</Text>
+          <Text style={styles.details}>total: $45</Text>
+        </Pressable>
+      </View>
+      <View style={styles.margins}>
+        <Button onPress={handleCheckout}>
+          <ButtonText>Checkout</ButtonText>
+        </Button>
+      </View>
     </View>
   );
 }
