549cecfeb2c7a6b934265f29ba4a31c5b2fa9562
Brian Ortega <brian.ort02@icloud.com>
2023-08-21 10:13:06 -0500

    working payments

--- app/(tabs)/profile/marketplace.tsx
+++ app/(tabs)/profile/marketplace.tsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
-import React, { useState } from "react";
+import { View, Text, StyleSheet, Pressable, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Button, ButtonText } from "@gluestack-ui/react";
 import axios from "axios";
 import { useStripe, StripeProvider } from "@stripe/stripe-react-native";
@@ -10,15 +10,22 @@ import {
   setDoc,
   getDoc,
   addDoc,
+  getDocs,
 } from "firebase/firestore";
 import { FIRESTORE_DB } from "@/config/FirebaseConfig";
 import { useAuth } from "@/context/AuthContext";
 
 export default function Marketplace() {
   const userId = useAuth().user?.uid;
-  console.log("userId", userId);
+
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
   const [isLoading, setLoading] = useState(false);
+  // const [paymentData, setPaymentData] = useState({
+  //   paymentIntentClientSecret: null,
+  //   ephemeralKeySecret: null,
+  //   customer: null,
+  // });
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -36,6 +43,7 @@ export default function Marketplace() {
     },
     details: {},
   });
+
   const createPaymentDocument = async (userId, paymentData) => {
     try {
       const paymentsCollectionRef = collection(
@@ -44,25 +52,54 @@ export default function Marketplace() {
       );
       await addDoc(paymentsCollectionRef, paymentData);
       console.log("Payment document created successfully");
+
+      // Optional: Update the Firestore document to trigger the Firestore function
+      const paymentDocRef = doc(
+        FIRESTORE_DB,
+        `stripe_customers/${userId}/payments`
+      );
+      await setDoc(paymentDocRef, paymentData); // This triggers the Firestore function
+      console.log("Firestore document updated to trigger function");
     } catch (error) {
       console.error("Error creating payment document:", error);
     }
   };
 
-  // Usage example
   const handleCheckout = async () => {
     try {
-      setLoading(true);
-      const paymentData = {
-        amount: 1000, // Example amount in cents
-        currency: "usd", // Example currency
-        // Add any other relevant payment data
-      };
-      await createPaymentDocument(userId, paymentData);
+      // 1. Create the payment document first
+      // const paymentData = {
+      //   amount: 12000, // Example amount in cents
+      //   currency: "usd", // Example currency
+      //   // ... any other relevant payment data
+      // };
+
+      // await createPaymentDocument(userId, paymentData);
+
+      // 2. Initialize the Payment Sheet
+      const { error } = await initPaymentSheet({
+        paymentIntentClientSecret:
+          "pi_3NhZhJIM1aKfC7VY16nkLDQA_secret_FMv6O7zyItfsuH1vSfdNY2kTr", // Replace with actual client secret
+      });
+
+      if (error) {
+        console.error("Error initializing payment sheet:", error);
+        return;
+      }
+
+      // 3. Present the Payment Sheet
+      const { error: paymentError } = await presentPaymentSheet();
+
+      if (paymentError) {
+        console.error("Error presenting payment sheet:", paymentError);
+        // Handle the payment error, show an alert, etc.
+        return;
+      }
+
+      // 4. Payment succeeded
+      console.log("Payment successful");
     } catch (error) {
-      console.error("Error initiating payment:", error);
-    } finally {
-      setLoading(false);
+      console.error("An error occurred:", error);
     }
   };
 
